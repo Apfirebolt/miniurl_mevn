a39eed6ebd9c1f363dda8e71536482877a4ab4cf
Apfirebolt <aspper20@gmail.com>
2025-03-01 14:01:38 +0530

    added url form

---
+++ client/src/components/UrlForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <img class="mx-auto h-36 w-48" src="../assets/1.png" alt="Workflow" />
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        Add Url
+      </h2>
+    </div>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <form class="space-y-6" @submit="handleSubmit">
+          <div>
+            <label
+              for="originalUrl"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Enter Url
+            </label>
+            <div class="mt-1">
+              <input
+                id="originalUrl"
+                name="originalUrl"
+                v-model="originalUrl"
+                type="text"
+                required=""
+                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <div class="flex justify-between">
+            <button
+              type="submit"
+              class="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Add Url
+            </button>
+
+            <button
+              @click="closeModalUtil"
+              class="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const originalUrl = ref("");
+const emits = defineEmits(["addUrlAction", "closeModal"]);
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  let payload = {
+    originalUrl: originalUrl.value,
+  };
+  emits("addUrlAction", payload);
+};
+
+const closeModalUtil = () => {
+  originalUrl.value = "";
+
+  emits("closeModal");
+};
+</script>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,28 +1,172 @@
 <template>
-    <header-component />
-    <section class="bg-white shadow sm:rounded-lg" id="about">
-        <div class="px-4 py-5 sm:p-6">
-            <h2 class="text-3xl my-5 text-center text-red-800">DASHBOARD</h2>
-            <p>
-                Futura is an expense tracker app that helps you keep track of your expenses. It is a simple and easy to use
-                app that helps you manage your expenses and keep track of your spending. You can add, edit and delete
-                expenses, view your expenses by category, and view your expenses by month.
-            </p>
-            <p class="mt-4">
-                The app is built using Vue.js, Tailwind CSS, and Express. It is a single page application that uses
-                Express back-end for authentication and MongoDB for data storage. The app is responsive and works on desktop,
-                tablet, and mobile devices.
-            </p>
+  <header-component />
+  <section class="bg-white shadow sm:rounded-lg" id="about">
+    <div class="px-4 py-5 sm:p-6">
+      <h2 class="text-3xl my-5 text-center text-red-800">DASHBOARD</h2>
+      <p>
+        Futura is an expense tracker app that helps you keep track of your
+        expenses. It is a simple and easy to use app that helps you manage your
+        expenses and keep track of your spending. You can add, edit and delete
+        expenses, view your expenses by category, and view your expenses by
+        month.
+      </p>
+      <p class="mt-4">
+        The app is built using Vue.js, Tailwind CSS, and Express. It is a single
+        page application that uses Express back-end for authentication and
+        MongoDB for data storage. The app is responsive and works on desktop,
+        tablet, and mobile devices.
+      </p>
+
+      <div class="mt-8">
+        <button
+          @click="openUrlModal"
+          class="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Add Url
+        </button>
+      </div>
+    </div>
+  </section>
+  <TransitionRoot appear :show="isUrlModalOpened" as="template">
+    <Dialog as="div" @close="closeUrlModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <div class="mt-2">
+                <UrlForm
+                  @add-url-action="addUrlActionUtil"
+                  @close-modal="closeUrlModal"
+                />
+              </div>
+            </DialogPanel>
+          </TransitionChild>
         </div>
-    </section>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+
+  <TransitionRoot appear :show="isConfirmModalOpened" as="template">
+    <Dialog as="div" @close="closeConfirmModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <div class="mt-2">
+                <ConfirmModal
+                  @confirm-action="confirmDelete"
+                  :message="deleteMessage"
+                  @close-modal="closeConfirmModal"
+                />
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
-import { useAuth } from '../store/auth';
+import { onMounted, ref, computed } from "vue";
+import AOS from "aos";
+import {
+  Dialog,
+  TransitionChild,
+  TransitionRoot,
+  DialogPanel,
+} from "@headlessui/vue";
+import { useAuth } from "../store/auth";
+import { useUrlStore } from "../store/url";
+import UrlForm from "../components/UrlForm.vue";
+import ConfirmModal from "../components/Confirm.vue";
+
 const auth = useAuth();
+const url = useUrlStore();
+const isUrlModalOpened = ref(false);
+const isConfirmModalOpened = ref(false);
+const deleteMessage = ref("");
+const selectedUrl = ref(null);
+
+const allUrls = computed(() => url.getUrls);
+const authData = computed(() => auth.getAuthData);
+
+const closeUrlModal = () => {
+  isUrlModalOpened.value = false;
+};
+
+const openUrlModal = () => {
+  isUrlModalOpened.value = true;
+};
+
+const openConfirmModal = () => {
+  isConfirmModalOpened.value = true;
+};
+
+const closeConfirmModal = () => {
+  isConfirmModalOpened.value = false;
+};
+
+const confirmDelete = async () => {
+  await url.deleteUrl(selectedUrl.value._id);
+  closeConfirmModal();
+};
+
+const addUrlActionUtil = async (urlData) => {
+  await url.addUrl(urlData);
+  closeUrlModal();
+};
 
 onMounted(async () => {
-    await auth.getProfileData();
+  AOS.init();
+  await url.getUrlsAction();
 });
 </script>
